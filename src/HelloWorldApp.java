f1874d1d1c229e2b807ce5e3c95e32961d723cad
DarrLet <user@MacBook-Pro-user.local>
2019-10-20 22:01:06 +0300

    hometask1

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,26 @@
+import sun.jvm.hotspot.HelloWorld;
+
+/**
+ * Homework1
+ */
+
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
+
+
+/**
+ * Homework 1.1
+ */
+
+
+
+
+
+/**
+ * Homework 2
+ */
